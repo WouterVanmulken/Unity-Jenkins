79f9424350addd0cc850bd649fd5e1db68e1a4a0
Vanmulken <Wouter.Vanmulken@amplexor.com>
2018-03-09 16:12:41 +0100

    Added EditorScript and default scene

---
+++ Assets/TestEditorScript.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+
+class TestEditorScript
+{
+    static string[] SCENES = FindEnabledEditorScenes();
+
+    static string APP_NAME = "TestProject";
+    static string TARGET_DIR = "target";
+
+    [MenuItem("Custom/CI/Build Windows")]
+    static void PerformWindowsBuild()
+    {
+        string target_dir = APP_NAME + ".exe";
+        GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
+
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+
+        return EditorScenes.ToArray();
+    }
+
+    static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+        string res = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options);
+        if (res.Length > 0)
+        {
+            throw new Exception("BuildPlayer failure: " + res);
+        }
+    }
+}
